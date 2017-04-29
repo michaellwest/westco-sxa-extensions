71bebe96dec747c32725dd13153a2b3ea3fb6ee0
Michael West <michaellwest@gmail.com>
2017-04-29 13:14:31 -0500

    Updated map to use correct POI field.

--- src/Feature/Westco.XA.Feature.Maps/Repositories/StaticMapsRepository.cs
+++ src/Feature/Westco.XA.Feature.Maps/Repositories/StaticMapsRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Script.Serialization;
+using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Data.Managers;
 using Sitecore.XA.Foundation.IoC;
@@ -23,11 +26,68 @@ namespace Westco.XA.Feature.Maps.Repositories
             }
         }
 
+        protected virtual Item GetPoiIcon(Item item)
+        {
+            string id = item[Sitecore.XA.Foundation.Geospatial.Templates.Poi.Fields.Type];
+            if (ID.IsID(id))
+                return this.ContentRepository.GetItem(new ID(id));
+            return (Item)null;
+        }
+
+        protected virtual void GetPois(Item item, List<Sitecore.XA.Feature.Maps.Models.Poi> result)
+        {
+            if (item == null)
+                return;
+            if (item.InheritsFrom(Sitecore.XA.Feature.Maps.Templates.PoiGroup.ID) || item.InheritsFrom(Sitecore.XA.Feature.Maps.Templates.PoiGroupingItem.ID))
+            {
+                foreach (Item child in item.GetChildren())
+                    this.GetPois(child, result);
+            }
+            else
+            {
+                if (!item.InheritsFrom(Sitecore.XA.Feature.Maps.Templates.MyLocationPoi.ID) && !item.InheritsFrom(Sitecore.XA.Foundation.Geospatial.Templates.IPoi.ID))
+                    return;
+                result.Add(new Sitecore.XA.Feature.Maps.Models.Poi(item, this.GetPoiIcon(item), null));
+            }
+        }
+
+        protected virtual List<Sitecore.XA.Feature.Maps.Models.Poi> Pois
+        {
+            get
+            {
+                var result = new List<Sitecore.XA.Feature.Maps.Models.Poi>();
+                if (this.Rendering.DataSourceItem != null)
+                {
+                    string str1 = this.Rendering.DataSourceItem[Templates.StaticMap.Fields.Poi];
+                    char[] chArray = new char[1] { '|' };
+                    foreach (string str2 in str1.Split(chArray))
+                    {
+                        if (ID.IsID(str2))
+                            this.GetPois(this.ContentRepository.GetItem(str2), result);
+                    }
+                }
+                return result;
+            }
+        }
+
+        protected virtual string GetJsonDataProperties()
+        {
+            var mode = this.MapMode.ToString();
+            var mapsProviderKey = this.GetMapsProviderKey(this.PageContext.Current);
+            var data = new
+            {
+                mode = mode,
+                Pois = this.Pois,
+                key = mapsProviderKey,
+            };
+            return new JavaScriptSerializer().Serialize((object)data);
+        }
+
         public override IRenderingModelBase GetModel()
         {
             var staticmapRenderingModel = new StaticMapRenderingModel();
             FillBaseProperties(staticmapRenderingModel);
-            //staticmapRenderingModel.JsonDataProperties = this.GetJsonDataProperties(ServiceLocator.Current.Resolve<ISiteInfoResolver>().GetHomeUrl(PageContext.Current));
+            staticmapRenderingModel.JsonDataProperties = GetJsonDataProperties();
             staticmapRenderingModel.Width =
                 Rendering.DataSourceItem[Sitecore.XA.Feature.Maps.Templates.Map.Fields.Width];
             staticmapRenderingModel.Height =
