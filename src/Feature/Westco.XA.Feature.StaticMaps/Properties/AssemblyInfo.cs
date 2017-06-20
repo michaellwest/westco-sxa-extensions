9bc537d87eb04ded3f660ff7c8c7a2e237ba77d8
Michael West <michaellwest@gmail.com>
2017-06-20 07:58:16 -0500

    Updated to version 1.1.

--- src/Feature/Westco.XA.Feature.StaticMaps/Properties/AssemblyInfo.cs
+++ src/Feature/Westco.XA.Feature.StaticMaps/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4afcf4a7-19c1-4fda-b831-97d5c11f46cf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
