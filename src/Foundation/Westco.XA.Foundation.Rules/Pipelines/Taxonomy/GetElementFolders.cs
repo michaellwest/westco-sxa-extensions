cdd26609b8fa6b61d0aeb6ceee6657403addea8f
Michael West <michaellwest@gmail.com>
2017-06-20 22:54:48 -0500

    #3 : Added ability to group rule definitions.

---
+++ src/Foundation/Westco.XA.Foundation.Rules/Pipelines/Taxonomy/GetElementFolders.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.Rules.Taxonomy;
+using Sitecore.Rules;
+using Sitecore.SecurityModel;
+
+namespace Westco.XA.Foundation.Rules.Pipelines.Taxonomy
+{
+    public class GetElementFolders : GetRuleElementProcessor
+    {
+        public List<string> Folders { get; set; } = new List<string>();
+
+        protected virtual IEnumerable<Item> GetConditionFolders(Item tag)
+        {
+            var database = tag.Database;
+
+            var elementFolderItems = new List<Item>();
+            foreach (var folder in Folders)
+            {
+                var elementsInFolders = GetElements(database, folder);
+                elementFolderItems.AddRange(elementsInFolders);
+            }
+            return elementFolderItems.Where(i =>
+            {
+                if (i.TemplateID == RuleIds.ElementFolderTemplateID)
+                    return i.Children.Any(f =>
+                    {
+                        if (f.TemplateID == RuleIds.TagDefinitionsFolderTemplateID)
+                            return f.Children.Any(k => k["Tags"].Contains(tag.ID.ToString()));
+                        return false;
+                    });
+                return false;
+            }).ToArray();
+        }
+
+        protected virtual IEnumerable<Item> GetElements(Database database, string rootPath)
+        {
+            IEnumerable<Item> elementFolders;
+            using (new SecurityDisabler())
+            {
+                elementFolders = database.SelectItems($"{rootPath}//*[@@templateid='{ RuleIds.ElementFolderTemplateID}']");
+            }
+            return elementFolders;
+        }
+
+        protected override void Action(Dictionary<ID, Item> result, RuleElementsPipelineArgs args)
+        {
+            foreach (Item tag in args.Tags.Values)
+            {
+                foreach (Item conditionFolder in this.GetConditionFolders(tag))
+                {
+                    if (!result.ContainsKey(conditionFolder.ID))
+                        result.Add(conditionFolder.ID, conditionFolder);
+                }
+            }
+        }
+
+        protected override Dictionary<ID, Item> GetResultCollection(RuleElementsPipelineArgs args)
+        {
+            return args.ElementFolders;
+        }
+    }
+}
